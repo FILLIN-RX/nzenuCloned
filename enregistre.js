7a36a0285b6a9d6799bd30ee9091655e000aa81d
RUXEL <djeutchouruxel@gmail.com>
2025-03-27 18:41:20 +0100

    entering the 'enregistrer domaine link'

---
+++ enregistre.js
@@ -0,0 +1,34 @@
+
+
+
+
+async function loaddomaine() {
+    const reponse= await fetch('domaine.json')
+    const domaine= await reponse.json()
+    const table = document.createElement('table');
+    const domainediv = document.getElementById('domaine')
+    for (let i = 0; i < domaine.length; i++) {
+
+        const domainelement = domaine[i];
+        const row =document.createElement('tr')
+        const domainetd = document.createElement('td')
+        domainetd.innerText=domainelement.domain
+        const newpricetd = document.createElement('td')
+        newpricetd.innerText=domainelement.newPrice
+        const  oldpriceTd = document.createElement('td')
+        oldpriceTd.innerText=domainelement.oldPrice
+        oldpriceTd.classList.add('old-price')
+        table.classList.add('col-md-12', 'col-lg-12','col-sm-12')
+        table.style.margin='0px';
+        row.appendChild(domainetd)
+        row.appendChild(newpricetd)
+        row.appendChild(oldpriceTd)
+        table.appendChild(row);
+        domainediv.appendChild(table)
+
+        
+    }
+
+    
+}
+loaddomaine()
